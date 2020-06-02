32a500b53e86da22ce5dac986c65cf6568aca4c0
Anatol Walger <anatol.walger@smail.th-koeln.de>
2020-06-02 15:42:20 +0200

    Initial Commit

---
+++ CoreDataTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by tolja on 02.06.20.
+//  Copyright © 2020 Walger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
