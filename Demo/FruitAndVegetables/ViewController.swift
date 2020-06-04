09ba2885e4983490297ec5e87f8a86af6e1de753
Anatol Walger <anatol.walger@smail.th-koeln.de>
2020-06-04 16:17:32 +0200

    CoreData Lösung Übung

--- Demo/FruitAndVegetables/ViewController.swift
+++ Demo/FruitAndVegetables/ViewController.swift
@@ -55,7 +55,7 @@ class ViewController: UIViewController, UITableViewDataSource {
                      }
         
     
-        self.tableView.reloadData()
+//        self.tableView.reloadData()
     }
 
     
